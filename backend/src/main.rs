1e3c6058a8b12a9e279a6d0fdb267d8e44d55c44
WalrusWalrus <walruscodes@users.noreply.github.com>
2021-02-09 12:18:55 -0800

    use actix-web, add simple hardcoded /list handler

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{middleware, web, App, HttpResponse, HttpServer, Responder};
+use serde::Serialize;
+use std::env;
+
+#[derive(Serialize)]
+struct Bin {
+    name: String,
+    id: String,
+}
+
+#[derive(Serialize)]
+struct BinList {
+    bins: Vec<Bin>,
+}
+
+/// Handler for "/list" - returns a list of bins.
+async fn handle_list() -> impl Responder {
+    let mut bin_list = vec![];
+    for i in 1..=3 {
+        bin_list.push(Bin {
+            name: i.to_string(),
+            id: format!("bin-{}", i),
+        });
+    }
+    HttpResponse::Ok().json(bin_list)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    if env::var_os("RUST_LOG").is_none() {
+        env::set_var("RUST_LOG", "actix_web=debug,actix_server=debug");
+    }
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .route("/list", web::get().to(handle_list))
+            .default_service(
+                web::resource("").route(web::to(|| HttpResponse::NotFound().body("nope"))),
+            )
+    })
+    .bind("0.0.0.0:3123")?
+    .run()
+    .await
 }
